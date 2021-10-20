18dff3ea0f48afdfc3190bafbf1fdc0afe377e20
Elena <alena1027@gmail.com>
2021-10-20 17:50:28 -0300

    feat: declare variables in js

---
+++ app.js
@@ -0,0 +1,8 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const container = document.querySelector('.container')
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+let activeSlideIndex = 0
